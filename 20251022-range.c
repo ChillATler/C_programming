58b67202b3edca312ace47ee22424f2052332073
Hansika Varma <hansikavarma7@gmail.com>
2025-11-25 06:52:21 +0530

    Add files via upload

---
+++ 20251022-range.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+int num;
+    printf("enter the number\n");
+    scanf ("%d",&num);
+    int min =num>=-3;
+    int max =num<=4;
+    int check =min && max;
+    printf("%d is in the range of -3 and 4 is %d ",num,check);
+}
