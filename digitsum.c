12c93b73115f5c4d55563361bbb0eef86383ca3c
Hansika Varma <hansikavarma7@gmail.com>
2025-12-01 07:13:12 +0530

    Add files via upload

---
+++ digitsum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{   int a;
+    printf("enter the number\n");
+    scanf ("%d",&a);
+    int sum =0;
+    while(a!=0)
+
+    {
+        int digit=a%10;
+        sum =sum+digit;
+        a=a/10;
+    }
+    printf("the sum of digits is %d\n",sum);
+
+}
